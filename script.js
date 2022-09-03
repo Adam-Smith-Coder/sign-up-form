19f1cc2423e9515347983b83a35f1655cad52079
Adam-Smith-Coder <adcollects@gmail.com>
2022-09-03 13:08:06 +0100

    Update files to include greater validation for password fields
    Update html file to remove pattern from password field as validation done in JS now
    Remove checkbox option for mailing list as it did not fit in well with the form layout and was surplus
    Add a span message with the details of what characters are required for the password field
    Add an empty span for a message to pop up advising the password is missing a certain item
    change the function call in the input from onkeyup to onblur
    
    Update js file with a check for password length of less than 8 to prompt a message
    Add a second check for length of 8 or more and then a check for the reg expression to validate
    the password
    If the password is neither of these is satisfied then run the comparison for both password fields

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const validStyles = `
-    border-radius: 7px;
-    background-color: green;
-    border-color: greenyellow;
-`;
-const invalidStyles = `
-    border-radius: 7px;
-    background-color: red;
-    border-color: orangered;
-`;
-
-
+const validationMessage = document.getElementById('validation-message')
 
 let validate = function() {
     let pass = document.getElementById('password').value
     let confPass = document.getElementById('conf-password').value
-    if (pass != confPass) {
+    let regEx = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+    
+    if (pass.length < 8) {
+        validationMessage.textContent = 'Password not long enough';
+
+    } else if (pass.length > 8 && !regEx.test(pass)) {
+        validationMessage.textContent = 'The password must contain 8 or more characters and include at least one number, and one uppercase and lowercase letter';
+    
+    } else {
+        if (pass != confPass) {
         document.getElementById("password").classList.add('invalidPass');
         document.getElementById("conf-password").classList.add('invalidPass');
-    } else {
+        validationMessage.textContent = 'Passwords do not match';
+        } else {
         document.getElementById("password").classList.remove('invalidPass');
         document.getElementById("password").classList.add('validPass');
         document.getElementById("conf-password").classList.remove('invalidPass')
         document.getElementById("conf-password").classList.add('validPass');
+        validationMessage.textContent = '';
+        }
     }
 }
-

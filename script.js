5119700f220a181b392ae2c3b8965a5b828f0766
Adam-Smith-Coder <adcollects@gmail.com>
2022-09-03 12:29:58 +0100

    update files with password validation coding
    Update html file with function to check that the values in the password
    fields match
    Update css file with new classes to get the valid and invalid colour options on passwords
    Update the js file with a function to validate the content of the password fields and apply
    a specific class if they are invalid or valid, when valid we need to remove the invalid class also

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-let validation = document.getElementsByClassName('.error')
+const validStyles = `
+    border-radius: 7px;
+    background-color: green;
+    border-color: greenyellow;
+`;
+const invalidStyles = `
+    border-radius: 7px;
+    background-color: red;
+    border-color: orangered;
+`;
+
+
+
+let validate = function() {
+    let pass = document.getElementById('password').value
+    let confPass = document.getElementById('conf-password').value
+    if (pass != confPass) {
+        document.getElementById("password").classList.add('invalidPass');
+        document.getElementById("conf-password").classList.add('invalidPass');
+    } else {
+        document.getElementById("password").classList.remove('invalidPass');
+        document.getElementById("password").classList.add('validPass');
+        document.getElementById("conf-password").classList.remove('invalidPass')
+        document.getElementById("conf-password").classList.add('validPass');
+    }
+}
+
